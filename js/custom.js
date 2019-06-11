558f684a1dcbf5f2972dedf2048d14d6a804272d
Mohit kumar <mohitsidhu157@gmail.com>
2019-06-11 19:30:52 +0530

    After adding carousel and cards

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 function showHeartset(){
     //document.getElementById("heartset").style.display = "none";
-    document.getElementById("heartset").src = "images/WhatsApp Image 2019-06-10 at 8.10.04 PM.jpeg";
+   /// document.getElementById("heartset").src = "images/1.jpeg";
     
 }
 function hideHeartset(){
     //console.log("Hello");
     
-    document.getElementById("heartset").src = "images/heartset.png";
+    //document.getElementById("heartset").src = "images/heartset.png";
                         
 }
