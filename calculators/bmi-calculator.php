dd328c7c04acf7d65a7633a04f8005b033abcd11
Mohit kumar <mohitsidhu157@gmail.com>
2019-06-14 22:48:23 +0530

    including php

---
+++ calculators/bmi-calculator.php
@@ -0,0 +1,17 @@
+<?php
+    include "header.php";
+?>
+<div class="container bmi-calculator">
+    <div class="col-md-6">
+    <div class="jumbotron">
+        <form action="" method="post">
+            Height : <input type="number" name="" id="">
+            Weight : <input type="number" name="" id="">
+            
+        </form>
+    </div>
+    </div>
+</div>
+<?php 
+    include "footer.php";
+?>
