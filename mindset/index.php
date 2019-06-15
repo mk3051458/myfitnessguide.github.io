d4f119842cf95620fa08cb37f2fc057dc3c6770d
Mohit kumar <mohitsidhu157@gmail.com>
2019-06-15 12:43:18 +0530

    adding mindset description

--- mindset/index.php
+++ mindset/index.php
@@ -1,8 +1,13 @@
 <?php
     include "header.php";
 ?>
-<div class="container-fluid description">
-
+<div class="container-fluid mindset-description">
+    <div class="row">
+        <div class="container">
+            <h3 class="text-center mindset-description-text">Keep your mind balanced</h3>
+            <div class="d-block mx-auto description-button">Get started</div>
+        </div>
+    </div>
 </div>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <?php
