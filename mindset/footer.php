699f68ba5d99e89496705b1dfe7c2a4f90cd61ca
Mohit kumar <mohitsidhu157@gmail.com>
2019-06-15 11:01:47 +0530

    adding mindset section

---
+++ mindset/footer.php
@@ -0,0 +1,14 @@
+   <div class="container-fluid footer text-center">
+        <div class="row  bg-dark text-white">
+            <h6 class="text-center d-block mx-auto">Copyright &copy; 2019 myfitnessguide.com. All rights reserved </h6>
+        </div>
+    </div>
+
+
+</body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</html>
---
+++ mindset/index.php
@@ -0,0 +1,10 @@
+<?php
+    include "header.php";
+?>
+<div class="container-fluid description">
+
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<?php
+    include "footer.php";
+?>
